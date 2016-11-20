4edd7dbe1b3719ff38cc38b1c6f4bec2ba1f9961
U-DESKTOP-M9MK2MS\Abby Caballero <abca9657@colorado.edu>
2016-11-19 21:04:14 -0700

    some adjustments

--- tst_run.m
+++ tst_run.m
@@ -1,10 +1,7 @@
 filename1 = 'face1.jpg';
 
 color_face = imread(filename1);
-tic
+
 Viola_Jones_img( color_face )
-toc
+
 gray_face  = rgb2gray(color_face);
-% gaus_1 = gaussFilter(color_face,1);
-% gaus_2 = gaussFilter(color_face,2);
-% gaus_10 = gaussFilter(color_face,10);
